53bd61a02ef6e24135b2b41c2cdf21c022441be0
User <User@DESKTOP-4E59VR3>
2024-10-31 22:46:20 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Practica09
+{
+    partial class TemperaturaSensor
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbllimtem = new System.Windows.Forms.Label();
+            this.lbltem = new System.Windows.Forms.Label();
+            this.lblcon = new System.Windows.Forms.Label();
+            this.lbltemperatura = new System.Windows.Forms.Label();
+            this.btndesconectar = new System.Windows.Forms.Button();
+            this.btnconectar = new System.Windows.Forms.Button();
+            this.txtescribir = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbllimtem
+            // 
+            this.lbllimtem.AutoSize = true;
+            this.lbllimtem.Font = new System.Drawing.Font("Gabriola", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbllimtem.Location = new System.Drawing.Point(58, 108);
+            this.lbllimtem.Name = "lbllimtem";
+            this.lbllimtem.Size = new System.Drawing.Size(177, 35);
+            this.lbllimtem.TabIndex = 0;
+            this.lbllimtem.Text = "Limete De Temperatura";
+            // 
+            // lbltem
+            // 
+            this.lbltem.AutoSize = true;
+            this.lbltem.Font = new System.Drawing.Font("Gabriola", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltem.Location = new System.Drawing.Point(445, 97);
+            this.lbltem.Name = "lbltem";
+            this.lbltem.Size = new System.Drawing.Size(111, 39);
+            this.lbltem.TabIndex = 1;
+            this.lbltem.Text = "Temperatura";
+            // 
+            // lblcon
+            // 
+            this.lblcon.AutoSize = true;
+            this.lblcon.Font = new System.Drawing.Font("Gabriola", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcon.Location = new System.Drawing.Point(89, 212);
+            this.lblcon.Name = "lblcon";
+            this.lblcon.Size = new System.Drawing.Size(146, 50);
+            this.lblcon.TabIndex = 2;
+            this.lblcon.Text = "Sin Conexion";
+            // 
+            // lbltemperatura
+            // 
+            this.lbltemperatura.AutoSize = true;
+            this.lbltemperatura.Font = new System.Drawing.Font("Microsoft YaHei UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltemperatura.Location = new System.Drawing.Point(437, 136);
+            this.lbltemperatura.Name = "lbltemperatura";
+            this.lbltemperatura.Size = new System.Drawing.Size(133, 86);
+            this.lbltemperatura.TabIndex = 3;
+            this.lbltemperatura.Text = "0.0";
+            // 
+            // btndesconectar
+            // 
+            this.btndesconectar.BackColor = System.Drawing.Color.Red;
+            this.btndesconectar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndesconectar.Location = new System.Drawing.Point(325, 288);
+            this.btndesconectar.Name = "btndesconectar";
+            this.btndesconectar.Size = new System.Drawing.Size(142, 38);
+            this.btndesconectar.TabIndex = 4;
+            this.btndesconectar.Text = "DESCONECTAR";
+            this.btndesconectar.UseVisualStyleBackColor = false;
+            this.btndesconectar.Click += new System.EventHandler(this.btndesconectar_Click);
+            // 
+            // btnconectar
+            // 
+            this.btnconectar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnconectar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnconectar.Location = new System.Drawing.Point(157, 288);
+            this.btnconectar.Name = "btnconectar";
+            this.btnconectar.Size = new System.Drawing.Size(142, 38);
+            this.btnconectar.TabIndex = 5;
+            this.btnconectar.Text = "CONECTAR";
+            this.btnconectar.UseVisualStyleBackColor = false;
+            this.btnconectar.Click += new System.EventHandler(this.btnconectar_Click);
+            // 
+            // txtescribir
+            // 
+            this.txtescribir.Location = new System.Drawing.Point(86, 157);
+            this.txtescribir.Name = "txtescribir";
+            this.txtescribir.Size = new System.Drawing.Size(124, 20);
+            this.txtescribir.TabIndex = 6;
+            this.txtescribir.TextChanged += new System.EventHandler(this.txtescribir_TextChanged);
+            // 
+            // TemperaturaSensor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orchid;
+            this.ClientSize = new System.Drawing.Size(660, 450);
+            this.Controls.Add(this.txtescribir);
+            this.Controls.Add(this.btnconectar);
+            this.Controls.Add(this.btndesconectar);
+            this.Controls.Add(this.lbltemperatura);
+            this.Controls.Add(this.lblcon);
+            this.Controls.Add(this.lbltem);
+            this.Controls.Add(this.lbllimtem);
+            this.Name = "TemperaturaSensor";
+            this.Text = "Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbllimtem;
+        private System.Windows.Forms.Label lbltem;
+        private System.Windows.Forms.Label lblcon;
+        private System.Windows.Forms.Label lbltemperatura;
+        private System.Windows.Forms.Button btndesconectar;
+        private System.Windows.Forms.Button btnconectar;
+        private System.Windows.Forms.TextBox txtescribir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica09
+{
+    public partial class TemperaturaSensor : Form
+    {
+        delegate void SetTextDelegate(string value);
+        public SerialPort ArduinoPort
+        {
+            get;
+        }
+
+        public TemperaturaSensor()
+        {
+            InitializeComponent();
+            ArduinoPort = new System.IO.Ports.SerialPort();
+            ArduinoPort.PortName = "COM5";
+            ArduinoPort.BaudRate = 9600;
+            ArduinoPort.DataBits = 8;
+            ArduinoPort.ReadTimeout = 1000;
+            ArduinoPort.WriteTimeout = 1000;
+            ArduinoPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            this.btnconectar.Click += btnconectar_Click;
+        }
+        private void DataReceivedHandler(object Sender, SerialDataReceivedEventArgs e)
+        {
+            string dato = ArduinoPort.ReadLine();
+            Escribirtxt(dato);
+        }
+        private void Escribirtxt(string dato)
+        {
+            if (InvokeRequired)
+                try
+                {
+                    Invoke(new SetTextDelegate(Escribirtxt), dato);
+                }
+                catch
+                {
+
+                }
+            else
+                lbltemperatura.Text = dato;
+        }
+
+        private void btnconectar_Click(object sender, EventArgs e)
+        {
+            btndesconectar.Enabled = true;
+            btnconectar.Enabled = false;
+            try
+            {
+                if (!ArduinoPort.IsOpen)
+                    ArduinoPort.Open();
+                if (int.TryParse(txtescribir.Text, out int temperatureLimmit))
+                {
+                    // Convierte el valor a una cadena y luego em un arreglo de bytes
+                    string limitString = temperatureLimmit.ToString();
+                    ArduinoPort.Write(limitString);
+                }
+                else
+                {
+                    MessageBox.Show("Ingrese un valor numero valido en el textbox");
+                }
+                lblcon.Text = "Conexion OK";
+                lblcon.ForeColor = System.Drawing.Color.LimeGreen;
+
+            }
+            catch
+            {
+                MessageBox.Show("Configure el puerto de comunicacion o desconecte");
+            }
+        }
+
+        private void btndesconectar_Click(object sender, EventArgs e)
+        {
+            btnconectar.Enabled = true;
+            btndesconectar.Enabled = false;
+            ArduinoPort.Close();
+            lblcon.Text = "Desconectado";
+            lblcon.ForeColor = System.Drawing.Color.Red;
+            lbltemperatura.Text = "00.0";
+        }
+
+        // Basura
+        private void txtescribir_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
